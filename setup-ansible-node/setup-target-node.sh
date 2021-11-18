09631a7125b390fd1a48f3ce3f39ebe50b24fefc
nasirkamal <nasirkamal818@gmail.com>
2021-11-18 12:38:09 +0500

    Update setup-target-node.sh

--- setup-ansible-node/setup-target-node.sh
+++ setup-ansible-node/setup-target-node.sh
@@ -5,5 +5,5 @@ source vars.txt
 username=${tnode_username:-"ansible"}
 password=${tnode_password:-"ansible"}
 
-.scripts/create-pwless-sudoer-user.sh $username $password
-.scripts/install-python.sh
+source ./scripts/create-pwless-sudoer-user.sh $username $password
+source ./scripts/install-python.sh
